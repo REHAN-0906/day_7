f956f4d54e78176bea93e54a56e8d3dfdb7446bd
rehan <rr1537460@gmail.com>
2025-07-05 16:31:07 +0530

    or operator

---
+++ logical.py
@@ -0,0 +1,12 @@
+Book=True
+Pen=False
+print(Book and Pen)
+Book=True
+Pen=True
+print(Book and Pen)
+Book=False
+Pen=False
+print(Book and Pen)
+Book=False
+Pen=True
+print(Book and Pen)
---
+++ logical_or.py
@@ -0,0 +1,12 @@
+Blue=True
+Black=False
+print(Blue or Black)
+Blue=True
+Black=True
+print(Blue or Black)
+Blue=False
+Black=False
+print(Blue or Black)
+Blue=False
+Black=True
+print(Blue or Black)
